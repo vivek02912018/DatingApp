1a4cf9bd45aa8b71a1a5aa0911ab5891f49a4920
vivek <visviv@gmail.com>
2020-04-22 20:00:48 +0530

    End of Section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+   [Authorize]
     [Route("api/[controller]")]// means whenever a request comes with /api/controllername it will hit here.
     [ApiController]
+   
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
@@ -28,6 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
